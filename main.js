d7d5badd2c0a318f13a3e64165bf294b33669e75
Anumnaseer <161718712+Anumnaseer@users.noreply.github.com>
2024-07-03 16:52:43 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+//question # 14
+var guestlist = ["affan", "anum", "ali", "aliyana", "subhan"];
+for (var i = 0; i < guestlist.length; i++) {
+    console.log("dear ".concat(guestlist[i], ":\n you are invited to dinner!\n "));
+}
---
+++ main.ts
@@ -0,0 +1,23 @@
+//question #11
+// array
+//defines the arrays of names
+var names = ["anum", "aiman", "aqib", "umair", "ambar"];
+//printing names one by one
+console.log(names[0]);
+console.log(names[1]);
+console.log(names[2]);
+console.log(names[3]);
+console.log(names[4]);
+//question no 12
+var Names = ["anum", "aiman", "aqib", "umair", "ambar"];
+console.log("hellow ".concat(Names[0], "!how are you doing today "));
+console.log("hellow ".concat(Names[1], "!how are you doing today "));
+console.log("hellow ".concat(Names[2], "!how are you doing today "));
+console.log("hellow ".concat(Names[3], "!how are you doing today "));
+console.log("hellow ".concat(Names[4], "!how are you doing today "));
+//question no 13
+var transportationmode = ["car", "motor cycle ", "bus ", " train "];
+for (var i = 0; i < transportationmode.length; i++) {
+    console.log(" i would like to own a ".concat(transportationmode[i]));
+}
+;
